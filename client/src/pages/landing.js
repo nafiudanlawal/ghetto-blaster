561db07ec93d01d39408c48ac50f4087d14496b9
Nafiu Lawal <nafiudanlawal@gmail.com>
2021-02-01 18:14:32 +0000

    [update] completed top section

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,6 +1,7 @@
 import Footer from "../components/landing_page/footer";
 import Top from "../components/landing_page/top";
-//import RoofSvg from "../components/landing_page/assets/roof.svg";
+import { ReactComponent as CircleShape } from "../components/landing_page/assets/Circle.svg";
+//import RoofSvg from "../components/landing_page/assets/roof.svg";client/src/components/landing_page/assets/Circle.svg
 
 function Home() {
   return (
@@ -8,7 +9,44 @@ function Home() {
         <Top />
         <div className="content">
             <section className="Top-section">
-                <img src="/roof.png"  alt="roof"/>
+                <div className="Shapes">
+                    <CircleShape className="Circle"/>
+                </div>
+                <div className="container">
+                    <div className="Left">
+                        <div className="Roof">
+                            <img src="/roof.png"  alt="roof"/>
+                        </div>
+                        <div className="Text">
+                            <div className="Heading">
+                                <h1>Home of your best music</h1>
+                            </div>
+                            <div className="Body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Right">
+                    <div className="Fun-img">
+                        <img src="/music1.jpg" alt="" />
+                    </div>
+                </div>
+                </div>
+            </section>
+            <section className="Middle-section">
+                <div className="Relax-img">
+                    <img src="/music2.png" alt="" />
+                </div>
+                <div className="Text">
+                    <div className="Heading">
+                        <h1>All Your Best Music</h1>
+                    </div>
+                    <div className="Body">
+                        <p>Find all your best music in one place. Find new music , classic music, live streams, album premieres, live studio sessions and interviews.</p>
+                    </div>
+                </div>
+            </section>
+            <section className="Bottom-section">
             <p>Landing page Here</p>
             </section>
         </div>
